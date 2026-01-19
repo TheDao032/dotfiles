4e42190a811a54913b1a5343b777225ec71fd016
TheDao032 <nthedao2705@gmail.com>
2026-01-19 11:45:28 +0700

    build: add render markdown package

--- macos/silicon/nvim/lua/modules/editor/package.lua
+++ macos/silicon/nvim/lua/modules/editor/package.lua
@@ -51,3 +51,14 @@ packadd({
     -- refer to the configuration section below
   },
 })
+
+packadd({
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  config = function()
+    require('render-markdown').setup({
+      completions = { lsp = { enabled = true } },
+    })
+  end,
+  opts = {},
+})
